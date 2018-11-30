c7fd3ecb3e17997fbc117619869d86550c76b259
Heorhii Dubinin <heorhii_dubinin@epam.com>
2018-11-30 18:41:01 +0200

    using AsyncTask

--- ninthTask/NewsInBroadcast/app/src/main/java/com/example/heorhii_dubinin/news/NewsActivity.java
+++ ninthTask/NewsInBroadcast/app/src/main/java/com/example/heorhii_dubinin/news/NewsActivity.java
@@ -1,14 +1,23 @@
 package com.example.heorhii_dubinin.news;
 
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class NewsActivity extends AppCompatActivity {
 
+    private static final String TAG = "News";
+    //    Button schedule;
+    Button cancel;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -19,15 +28,55 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = findViewById(R.id.recycler);
-        mBrakingNews = QueryUtils.extractNews();
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mRecyclerView = findViewById(R.id.recycler);
+//        mBrakingNews = QueryUtils.extractNews();
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//
+//        mLayoutManager = (new LinearLayoutManager(this));
+//        mAdapter = new NewsAdapter(mBrakingNews, this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mRecyclerView.setAdapter(mAdapter);
+//        mAdapter.notifyDataSetChanged();
+        scheduleJob(/*v*/);
 
-        mLayoutManager = (new LinearLayoutManager(this));
-        mAdapter = new NewsAdapter(mBrakingNews, this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-        mAdapter.notifyDataSetChanged();
+//        schedule = findViewById(R.id.schedule);
+//        schedule.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                scheduleJob(v);
+//            }
+//        });
+
+        cancel = findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelJob(v);
+            }
+        });
+    }
+
+    public void scheduleJob(/*View view*/) {
+        ComponentName componentName = new ComponentName(this, MyJobService.class);
+        JobInfo info = new JobInfo.Builder(321, componentName)
+                .setRequiresCharging(false)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+        JobScheduler newsScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = newsScheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job scheduled");
+        } else {
+            Log.d(TAG, "Job scheduling failed");
+        }
+    }
+
+    public void cancelJob(View view) {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "Job cancelled");
     }
 }
